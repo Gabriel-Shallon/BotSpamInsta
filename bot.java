f1ec87d6a97519e24b0f21301c1d7b00d353d8e6
Gabriel-Shallon <shallonhatake@gmail.com>
2023-03-31 11:43:25 -0400

    Update bot.java

--- bot.java
+++ bot.java
@@ -5,46 +5,48 @@ import java.awt.event.KeyEvent;
 public class bot {
 
     private Robot robot;
-    private int delay; 
-    private int repeat; 
-    private String text; 
-  
-
+    private int delay;
+    private int repeat;
+    
+    private String text;
 
     public bot(int delay, int repeat, String text) throws AWTException {
         robot = new Robot();
         this.delay = delay;
         this.repeat = repeat;
+       
         this.text = text;
-        
+
     }
 
     public void run() throws InterruptedException {
-        
+
         for (int i = 0; i < repeat; i++) {
-            robot.delay(delay); 
-            
-            robot.mousePress(KeyEvent.BUTTON1_DOWN_MASK); 
-            robot.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK); 
-            robot.delay(delay); 
-           
-            for (char c : text.toCharArray()) { 
+            robot.delay(delay);
+
+            robot.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
+            robot.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
+            robot.delay(delay);
+
+
+            robot.keyPress(KeyEvent.VK_SHIFT); 
+            robot.keyPress(KeyEvent.VK_3); 
+            robot.keyRelease(KeyEvent.VK_3); 
+            robot.keyRelease(KeyEvent.VK_SHIFT);
+
+
+            for (char c : (text).toCharArray()) {
                 robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
-                robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c)); 
+                robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
             }
-            robot.delay(1); 
-            
-            robot.keyPress(KeyEvent.VK_ENTER); 
-            robot.keyRelease(KeyEvent.VK_ENTER); 
-   
+            robot.delay(1);
+
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+
         }
     }
 }
-        
-       
-        
-        
-        
-        
-        
-        
+
+
+
