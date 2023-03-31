7cdebcd305853bad294da0f63e79bc94f6fe6285
Gabriel-Shallon <shallonhatake@gmail.com>
2023-03-31 11:43:28 -0400

    Update botexe.java

--- botexe.java
+++ botexe.java
@@ -1,20 +1,19 @@
 import java.awt.AWTException;
 
-public class botexe {
-    
-    public static void main(String[] args) {
-        try {
-            int delay = 1000; // intervalo entre os cliques
-            int repeat = 9999;
+public class botexe{
 
-            ; //repetições
-            String text = "MARCELO"; //texto
-            bot clicker = new bot(delay, repeat, text); 
-            clicker.run(); 
-        } catch (AWTException | InterruptedException e) {
-            e.printStackTrace();
+            public static void main(String[] args) {
+            try {
+
+                int delay = 1000; // intervalo entre os cliques
+                int repeat = 9999; //repetições
+                String text = "info"; //texto
+                 //hashtag
+                bot clicker = new bot(delay, repeat, text); //adiciona a hashtag antes do texto
+                clicker.run(); 
+            } catch (AWTException | InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
-
-
-}
+    
