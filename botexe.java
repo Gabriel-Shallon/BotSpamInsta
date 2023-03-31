aaadf02a56f975f576bf23b181cdaf5161b08810
Gabriel-Shallon <shallonhatake@gmail.com>
2023-03-31 01:26:19 -0400

    Update botexe.java

--- botexe.java
+++ botexe.java
@@ -5,8 +5,10 @@ public class botexe {
     public static void main(String[] args) {
         try {
             int delay = 1000; // intervalo entre os cliques
-            int repeat = 5; //repetições
-            String text = "Te amo Denilson"; //texto
+            int repeat = 9999;
+
+            ; //repetições
+            String text = "MARCELO"; //texto
             bot clicker = new bot(delay, repeat, text); 
             clicker.run(); 
         } catch (AWTException | InterruptedException e) {
